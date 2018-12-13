d9f88e5f47f793e744606b794bad99f633519f31
Kawah Wong <kawah.wong.w@gmail.com>
2018-12-12 16:19:30 -0800

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,30 +27,30 @@ connection.connect(function(err) {
         {
             name: "whichID",
             type: "input",
-            message: "What is the ID associated with the product you want to buy?"
+            message: "What is the ID from the product you want to buy?"
         },
         {
             name: "quantity",
             type: "input",
-            message: "How much of the product are you looking to buy?"
+            message: "How much of the product are you going to buy?"
         }])
         .then(function (results) {
             var totalCost = 0;
-            // define query to reduce quantity
+           
             var query = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ? AND stock_quantity > ?";
-            // query to carry out function and reduce quantity
+            
             connection.query(query, [results.quantity, results.whichID, results.quantity], function (err, res) {
             });
-            // query to update the product sales
+            
             connection.query("UPDATE products SET product_sales = product_sales + price * ? WHERE id = ?", [results.quantity, results.whichID], function (err, target) {
             })
-            // print that transaction was successful and show altered database
+           
             connection.query("SELECT * FROM products WHERE id = ?",[results.whichID], function (err, result) {
                 console.table(result);
                 console.log("Purchase of " + results.quantity + " " + result[0].product_name + " successful. Your total cost was: $" + result[0].price * results.quantity);
             })
 
-            // end connection
+            
             connection.end();
         });
 };
